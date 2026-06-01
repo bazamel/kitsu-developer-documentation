7bb34b390d49b158a6904ef284be024b20c98adf
bazamel <basunako@gmail.com>
2026-06-01 17:42:46 +0700

    added signup CTA & matomo script

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
 import Mermaid from "./Mermaid.vue";
+import KitsuLayout from "./KitsuLayout.vue"
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
+  Layout: KitsuLayout,
   enhanceApp({ app, router }) {
     app.component("Mermaid", Mermaid);
   },
